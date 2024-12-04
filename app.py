ca0789db9c89dc6cec336966e2e2084e02613de8
Fernando162021 <fernandoquinteromps@outlook.com>
2024-12-04 17:13:10 -0600

    final

--- app.py
+++ app.py
@@ -7,10 +7,6 @@ app = Flask(__name__)
 def saludo():
     return jsonify({'mensaje': 'Hola Mundo'}), 200
 
-@app.route('/despedida', methods=['GET'])
-def despedida():
-    return jsonify({'mensaje': 'Adiós Mundo'}), 200
-
 # Ejecución de la app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
