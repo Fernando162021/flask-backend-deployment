d448cac01b8563fff82028ae333e66ba57872fcf
Fernando162021 <fernandoquinteromps@outlook.com>
2024-12-04 10:22:14 -0600

    minor chages

--- app.py
+++ app.py
@@ -7,6 +7,10 @@ app = Flask(__name__)
 def saludo():
     return jsonify({'mensaje': 'Hola Mundo'}), 200
 
+@app.route('/depedida', methods=['GET'])
+def despedida():
+    return jsonify({'mensaje': 'Adiós Mundo'}), 200
+
 # Ejecución de la app
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=5000)
